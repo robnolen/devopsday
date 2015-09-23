7eee1e0f4599100622344b571e4c796614d08fe8
Rob Nolen <rob.nolen@gmail.com>
2015-09-22 22:12:14 -0700

    fixed and added things needed for CF

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import redis
+import json
 from flask import Flask
 from flask import request
 from kairos import Timeseries
@@ -13,9 +14,9 @@ redis_pw   = os.getenv('REDIS_PW')
 
 #if VCAP_SERVICES is not Nil, then we are in CF, and use rediscloud service
 if not os.getenv('VCAP_SERVICES') == None:
-    service_data = json.loads(os.getenv('VCAP_SERVICES'))['rediscloud'][0]
+    service_data = json.loads(os.getenv('VCAP_SERVICES'))['redis'][0]
     service_creds = service_data['credentials']
-    redis_host = service_creds['hostname']
+    redis_host = service_creds['host']
     redis_port = service_creds['port']
     redis_pw   = service_creds['password']
 
